e78f9958c6ddecc860ec125967c2bfe140c745ec
Sacha Morgese <sachamorgese@gmail.com>
2018-02-10 11:03:42 +0900

    Working on the Picker

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Picker } from 'react-native';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
@@ -24,8 +25,21 @@ class EmployeeCreate extends Component {
             onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
           />
         </CardSection>
-
         <CardSection>
+          <Picker>
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+        <CardSection>
+          <Button>
+            Create
+          </Button>
         </CardSection>
       </Card>
     );
@@ -35,6 +49,6 @@ class EmployeeCreate extends Component {
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employees;
   return { name, phone, shift };
-}
+};
 
 export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
